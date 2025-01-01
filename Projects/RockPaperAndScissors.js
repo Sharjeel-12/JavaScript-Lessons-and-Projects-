b213ffe8e8a2146d46de82dbf6a7cc3ac2f07523
Muhammad Sharjeel Farzad <msharjeelfarzad@gmail.com>
2025-01-01 10:51:27 +0500

    Update Code with User Interface

--- Projects/RockPaperAndScissors.js
+++ Projects/RockPaperAndScissors.js
@@ -1,12 +1,41 @@
-console.log("*************Welcome to ROCK PAPER and SCISSORS!*************");
+console.log("*************  Welcome to ROCK PAPER and SCISSORS!  *************");
+//Define the Scores of Computer and Player
+
+// this is the UI Branch
 let ComputerScore=0; let HumanScore=0;
+let imageScrolling=0;
+let rounds=0;let starter=0;
+let scoreBoard=document.getElementById("ScoreBoard");
+let startBtn=document.getElementById("start");
+let img_1=document.getElementById("img-1");
+let img_2=document.getElementById("img-2");
+// let div_1=document.getElementById("C1");
+img_1.style.display="none";
+img_2.style.display="none";
+function setImg(choice){
+    img_1.style.display="block";
+    if (choice=="rock"){
+        
+        img_1.src="rock-paper-scissors-296854_1920.png"
+    }
+    if (choice=="paper"){
+        
+        img_1.src="rock-paper-scissors-296855_1920.png"
+    }
+    if (choice=="scissors"){
+        img_1.src="rock-paper-scissors-296853_1920.png"
+    }
+    
+}
 
-
-
-function getHumanChoice(){
-let choice= window.prompt("Enter your choice of move");
+function getHumanChoice(btn){
+// let choice= window.prompt("Enter your choice of move");
+let choice=btn.getAttribute("id");
 choice=choice.toLowerCase();
+// console.log(choice);
+setImg(choice);
 if(choice=="rock" | choice=="paper" | choice=="scissors"){
+    
     return choice;
 }
 else{
@@ -21,19 +50,24 @@ function getComputerChoice(){
     let choice=Math.random();
     if (choice<=0.33){
         choice="rock";
+        img_2.src="rock-paper-scissors-296854_1920.png"
     }
     else if (choice>0.33 && choice<=0.66){
         choice="paper";
+        img_2.src="rock-paper-scissors-296855_1920.png"
     }
     else if (choice>0.66 && choice<=1){
         choice="scissors";
+        img_2.src="rock-paper-scissors-296853_1920.png"
     }
+    clearInterval(imageScrolling);
     return choice;
 }
 
 
 
 function ScoreDecider(HumanChoice,CompChoice){
+
     if (HumanChoice=="rock"){
         if (CompChoice=="paper"){
             ComputerScore++;
@@ -71,44 +105,112 @@ function ScoreDecider(HumanChoice,CompChoice){
 }
     }
 
-    return {ComputerScore,HumanScore} ;
-}
-
-
-
-
-
-function PlayRound(){
-    let CompChoice=getComputerChoice();
-    let HumanChoice=getHumanChoice();
-    let Score=ScoreDecider(HumanChoice,CompChoice);
-    let CompScore=Score.ComputerScore;
-    let HumScore=Score.HumanScore;
     console.log(CompChoice,HumanChoice);
-    console.log(CompScore,HumScore);
-    console.log("********************************");
+    console.log(ComputerScore,HumanScore);
+    scoreBoard.innerHTML=""
+    scoreBoard.innerHTML+=`<h1 class="center">SCORE BOARD</h1><p>Your Score:<span >${HumanScore}</span> </p> 
+    <p>Computer Score: <span >${ComputerScore}</span></p>`
+    rounds++;
+    
+    console.log(rounds);
+    if (rounds%5==0){
+        
+        endGame();
+    }
+    else{
+        stopRound();
+    }
+    // Start();
 }
 
+function stopRound(){
+    clearInterval(imageScrolling);
+    let nxt=document.getElementById("nextRound");
+    nxt.addEventListener("click",newRound);
+    nxt._listener=newRound;
+    let PlayBtns=document.querySelectorAll(".Playbtn");
+    PlayBtns.forEach((btn)=>{
+        play=btn._listener;
+        btn.removeEventListener("click",play);
+        
+    })
+}
+function newRound(){
+    imageScrolling= setInterval(imageScroll,10);
+    let nxt=document.getElementById("nextRound");
+    newRnd=nxt._listener;
+    nxt.removeEventListener("click",newRnd);
+    let PlayBtns=document.querySelectorAll(".Playbtn");
+    PlayBtns.forEach((btn)=>{
+        play=btn._listener;
+        btn.addEventListener("click",play);
+        
+    })
+}
+
+function endGame(){
+    startBtn.addEventListener("click",Start);
+    startBtn.innerHTML="Re-Start";
+    let PlayBtns=document.querySelectorAll(".Playbtn");
+    PlayBtns.forEach((btn)=>{
+        play=btn._listener;
+        btn.removeEventListener("click",play);
+        
+    })
 
 
-function PlayGame(){
-    PlayRound();
-    PlayRound();
-    PlayRound();
-    PlayRound();
-    PlayRound();
-    if (HumanScore>ComputerScore){
-        console.log("Game Over and You Win !!!!!!")
+        if (HumanScore>ComputerScore){
+       console.log("<p>Game Over and You Win !!!!!!</p>")
+       scoreBoard.innerHTML+=`<p>Game Over and You Win !!!!!!</p>`;
     }
     if (HumanScore==ComputerScore){
-        console.log("Game Over. It is a TIE!");
+        console.log("<p>Game Over. It is a TIE!");
+        scoreBoard.innerHTML+=`<p>Game Over. It is a TIE!</p>`;
     }
     if (HumanScore<ComputerScore){
-        console.log("YOU LOST !!!!!!")
+        console.log("<p>YOU LOST !!!!!!<p>");
+        scoreBoard.innerHTML+=`<p>YOU LOST !!!!!!</p>`;
+
     }
+
 }
 
 
 
 
-PlayGame();
+function PlayRound(btn){
+    
+   return ()=> ScoreDecider(getHumanChoice(btn),getComputerChoice())
+}
+
+function imageScroll(){
+    img_2.style.display="block";
+    let choice=Math.random();
+    if (choice<=0.33){
+        img_2.src="rock-paper-scissors-296855_1920.png";
+    }
+    else if (choice>0.33 && choice<=0.66){
+        img_2.src="rock-paper-scissors-296854_1920.png";
+    }
+    else if (choice>0.66 && choice<=1){
+        img_2.src="rock-paper-scissors-296853_1920.png";
+    }
+
+}
+function Start(){
+    scoreBoard.innerHTML=`<h1 class="center">SCORE BOARD</h1>`;
+    startBtn.removeEventListener("click",Start);
+        imageScrolling= setInterval(imageScroll,10);
+    let PlayBtns=document.querySelectorAll(".Playbtn");
+    PlayBtns.forEach((btn,index)=>{
+        // btn.addEventListener("click",()=>getHumanChoice(btn));
+        const play=PlayRound(btn);
+        btn._listener=play;
+        btn.addEventListener("click",play);
+    })
+
+}
+
+
+    startBtn.addEventListener("click",Start);
+   
